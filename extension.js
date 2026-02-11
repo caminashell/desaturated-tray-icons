6647e4d97150cee72882d242314b78f44aef1d48
C Λ M I N Λ S H Ξ L L <92443358+caminashell@users.noreply.github.com>
2026-02-11 13:34:43 +0000

    geometry check to mitigate mutter render warning

--- extension.js
+++ extension.js
@@ -1,43 +1,54 @@
-import Clutter from 'gi://Clutter';
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import Clutter from "gi://Clutter";
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
 
 export default class Extension {
-    constructor() {
-    }
+  constructor() {}
 
-    enable() {
-        this._child_added_signal_id = Main.panel._rightBox.connect(
-            'child-added', this._child_added_event_handler
-        )
-        this._add_effects();
-    }
+  enable() {
+    this._child_added_signal_id = Main.panel._rightBox.connect(
+      "child-added",
+      this._child_added_event_handler,
+    );
+    this._add_effects();
+  }
 
-    disable() {
-        Main.panel._rightBox.disconnect(this._child_added_signal_id);
-        this._remove_effects();
-    }
+  disable() {
+    Main.panel._rightBox.disconnect(this._child_added_signal_id);
+    this._remove_effects();
+  }
 
-    _child_added_event_handler(_, child) {
+  _dimension_check(child) {
+    if (child.width > 0 && child.height > 0) return true;
+    return false;
+  }
+
+  _child_added_event_handler(_, child) {
+    if (this._dimension_check(child)) {
+      child.add_effect(new Clutter.DesaturateEffect());
+    }
+  }
+
+  _get_tray() {
+    return Main.panel._rightBox.get_children();
+  }
+
+  _add_effects() {
+    for (let child of this._get_tray()) {
+      if (this._dimension_check(child)) {
         child.add_effect(new Clutter.DesaturateEffect());
+      }
     }
+  }
 
-    _get_tray() {
-        return Main.panel._rightBox.get_children();
-    }
-
-    _add_effects() {
-        for (let child of this._get_tray()) {
-            child.add_effect(new Clutter.DesaturateEffect());
+  _remove_effects() {
+    for (let child of this._get_tray()) {
+      for (let effect of child.get_effects()) {
+        if (effect instanceof Clutter.DesaturateEffect) {
+          if (this._dimension_check(child)) {
+            child.remove_effect(effect);
+          }
         }
+      }
     }
-
-    _remove_effects() {
-        for (let child of this._get_tray()) {
-            for (let effect of child.get_effects()) {
-                if (effect instanceof Clutter.DesaturateEffect) {
-                    child.remove_effect(effect);
-                }
-            }
-        }
-    }
+  }
 }
